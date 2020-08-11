15224d3bc19255f33a8b4a0ee2e99efcafeb0d7b
Mayur Walzade <mayur858559@yahoo.co.inm>
2020-08-12 02:45:41 +0530

    style changes

--- ecomerece-site-app/src/component/Product.js
+++ ecomerece-site-app/src/component/Product.js
@@ -14,18 +14,26 @@ export default class Product extends Component {
                     <Link to="/details">
                     <img src={img} alt="product" className="card-img-top"/>
                     </Link>
-                    <button className="cart-btn" disabled={inCart ? true : false}
+                    <button className="cart-btn mt-2" disabled={inCart ? true : false}
                         onClick={() => {
                            console.log("added to the cart");     
-                        }}/>
+                        }}>
                     {inCart ? (
                         <p className="text-capitalize mb-0" disabled>
                         {""}
-                        in inCart
+                        in cart
                          </p> 
                     ):(
                         <i className="faas fa-cart-plus"/>
                     )}
+                    </button>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">{title}</p>
+                        <h5 className="text-blue font-italic mb-0">
+                        <span className="mr-1">$</span>
+                        {price}
+                        </h5>
                     </div>
                     </div>
                 
@@ -34,4 +42,48 @@ export default class Product extends Component {
     }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+.card{
+    border-color:transparent;
+    transition:all 1s linear;
+}
+.card-footer{
+    background:transparent;
+    border-top:transparent;
+    transition:all 1s linear;
+}
+&:hover{
+    .card{
+        border:0.04rem solid rgba(0,0,0.2);
+        box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer{
+        background: rgba(247,247,247);
+    }
+}
+.img-container{
+    position:relative;
+    overflow:hidden;
+}
+.carrd-img-top{
+    transition: all 1s linear
+}
+.img-container:hover .card-img-top{
+    transform:scale(1.2);
+}
+.cart-btn{
+    position:absolute;
+    bottom:0;
+    right:0;
+    padiing:0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0 :
+    transform: translate(100%, 100%);
+}
+.img-container:hover .cart-btn{
+    transform: translate(0,0);
+}
+`;
--- ecomerece-site-app/src/data.js
+++ ecomerece-site-app/src/data.js
@@ -1,7 +1,7 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "Charlotte's Web",
     img: "img/product-1.png",
     price: 10,
     company: "GOOGLE",
@@ -13,7 +13,7 @@ export const storeProducts = [
   },
   {
     id: 2,
-    title: "Samsung S7",
+    title: "Mieko and the Fifth Treasure",
     img: "img/product-2.png",
     price: 16,
     company: "SAMSUNG",
@@ -25,7 +25,7 @@ export const storeProducts = [
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
+    title: "The Outsiders – S.E",
     img: "img/product-3.png",
     price: 8,
     company: "htc",
@@ -37,7 +37,7 @@ export const storeProducts = [
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "The House On Mango Street",
     img: "img/product-4.png",
     price: 18,
     company: "htc",
@@ -49,7 +49,7 @@ export const storeProducts = [
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
+    title: "Thirteen Reasons Why",
     img: "img/product-5.png",
     price: 24,
     company: "htc",
@@ -61,7 +61,7 @@ export const storeProducts = [
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "Peter Pan – J.M",
     img: "img/product-6.png",
     price: 17,
     company: "apple",
@@ -73,7 +73,7 @@ export const storeProducts = [
   },
   {
     id: 7,
-    title: "Iphone 7",
+    title: "The Old Man and the Sea",
     img: "img/product-7.png",
     price: 30,
     company: "apple",
@@ -85,7 +85,7 @@ export const storeProducts = [
   },
   {
     id: 8,
-    title: "Smashed Iphone",
+    title: "The Old Man and the Sea",
     img: "img/product-8.png",
     price: 2,
     company: "apple",
@@ -99,7 +99,7 @@ export const storeProducts = [
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
+  title: "The Giver ",
   img: "img/product-1.png",
   price: 10,
   company: "google",
